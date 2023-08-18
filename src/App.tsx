91f12d27ab56ae6731846bca2df58500ffb8bf22
JK <viletert@gmail.com>
2023-08-18 14:36:33 +0300

    comment removal

--- src/App.tsx
+++ src/App.tsx
@@ -11,12 +11,7 @@ function App() {
             {/*Article 1*/}
             <Accordion titleValue={'Menu'} collapsed={false}/>
             <Accordion titleValue={'Users'} collapsed={false}/>
-            {/*<Rating value={0}/>*/}
-            {/*<Rating value={1}/>*/}
-            {/*<Rating value={2}/>*/}
-            {/*<Rating value={3}/>*/}
-            {/*<Rating value={4}/>*/}
-            {/*<Rating value={5}/>*/}
+
         </div>
     );
 }
