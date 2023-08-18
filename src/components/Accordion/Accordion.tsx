ac82c46e4125c9dc96fb7b9e5d9ad817305875db
JK <viletert@gmail.com>
2023-08-18 14:20:21 +0300

    commit

---
+++ src/components/Accordion/Accordion.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+
+type AccordionPropsType = {
+    titleValue: string
+    collapsed: boolean
+}
+
+function Accordion(props: AccordionPropsType) {
+    if(props.collapsed) {
+        return <div>
+            <AccordionTitle title={props.titleValue}/>
+        </div>
+    } else {
+        return <div>
+            <AccordionTitle title={props.titleValue}/>
+            <AccordionBody title={props.titleValue}/>
+        </div>
+    }
+
+}
+
+type AccordionTitlePropsType = {
+    title: string
+}
+
+function AccordionTitle(props: AccordionTitlePropsType) {
+    console.log("AccordionTitle rendering")
+    return (
+        <h3>--{props.title}--</h3>
+    )
+}
+
+type AccordionBodyPropsType = {
+    title: string
+}
+
+function AccordionBody(props: AccordionBodyPropsType) {
+    console.log("Star rendered")
+    return <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+    </ul>
+}
+
+export default Accordion;
